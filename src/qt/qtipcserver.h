90487aa990c6daa6963fdadabbf655f23941e18d
Deacoin <learnbtc42@gmail.com>
2017-04-24 14:13:04 +0000

    dea coin

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define dea-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "deaURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
